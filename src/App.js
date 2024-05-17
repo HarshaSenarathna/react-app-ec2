71e735780626f6d171908aefa167368067ad7a3c
HarshaSenarathna <hsenarathna1999@gmail.com>
2024-05-17 15:01:34 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import WelcomeScreen from './components/WelcomeScreen';
+function App() {
+  return (
+    <div className="App">
+      <WelcomeScreen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WelcomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/WelcomeScreen.css';
+
+const WelcomeScreen = () => {
+  return (
+    <div className="welcome-container">
+      <div className="neon-box">
+        <h1 className="welcome-title">Welcome to Future</h1>
+        <p className="welcome-subtitle">Embrace the Future</p>
+        <button className="start-button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
