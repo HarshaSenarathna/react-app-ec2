d0d090fbdf747ec7e22ec39001d0c8dec2960a20
HarshaSenarathna <hsenarathna1999@gmail.com>
2024-05-18 20:35:03 +0530

    testing the pull

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -6,7 +6,7 @@ const WelcomeScreen = () => {
     <div className="welcome-container">
       <div className="neon-box">
         <h1 className="welcome-title">Welcome to Future</h1>
-        <p className="welcome-subtitle">Embrace the Future</p>
+        <p className="welcome-subtitle">Embrace the Future By Harsha</p>
         <button className="start-button">Get Started</button>
       </div>
     </div>
